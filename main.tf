33a5da4845f692c8bedd25fee098064f11cfa594
pavan2704 <138353777+pavanteja2704@users.noreply.github.com>
2024-06-07 15:48:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "default" {
  
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"// RHEL 9 image
+      image = "debian-cloud/debian-11"
       size  = 50 // 50 GB boot disk
     }
   }
